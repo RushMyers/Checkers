ff1225d21567e99b14ee67456c58293d41628f06
rush <myersrush1986@gmail.com>
2018-03-01 21:47:59 -0500

    board object created as well as checker class

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
-  setBoard();
-  handleClicks();
+  gameBoard.create();
+
+  // $('.tile').click(function(event){
+  //   let tile = event.currentTarget;
+  //   if((tile.classList.contains('red') && game.turn === 'player1') ||
+  //      (tile.classList.contains('white') && game.turn === 'player2')) {
+  //       Checker.selectChecker(tile);
+  //   }
+  // });
 });
 
-var gameBoard = [
+var tiles = [];
+var checkers = [];
+
+function Checker(color, position) {
+  this.color = color;
+  this.player = '';
+  this.color === 'red' ? this.player = 'player1' : this.player = 'player2';
+  this.position = position;
+  this.selectChecker = function(checker) {
+    console.log(checker);
+    tile.classList.add('isSelected');
+  }
+}
+
+var gameBoard = {
+  board: [
     [ 0, 2, 0, 2, 0, 2, 0, 2],
     [ 2, 0, 2, 0, 2, 0, 2, 0],
     [ 0, 2, 0, 2, 0, 2, 0, 2],
@@ -12,66 +34,74 @@ var gameBoard = [
     [ 1, 0, 1, 0, 1, 0, 1, 0],
     [ 0, 1, 0, 1, 0, 1, 0, 1],
     [ 1, 0, 1, 0, 1, 0, 1, 0],
-]
-
-var tiles = [];
-
-function handleClicks(){
-  $('div.white-checker').on("click", function( event) { selectChecker(event.target); });
-  $('div.white-checker').on("click", function( event) { selectChecker(event.target); });
-}
-
-function setBoard() {
-  let tileNo = 0;
-  gameBoard.forEach(function(row, i) {
-    row.forEach(function(column, j) {
-      if (i%2 === 0) {
-        if(j%2 === 1 && column == 2) {
-          $('.board').append("<div class='tile dark white' id="+tileNo+"></div>");
-          tiles[tileNo] = [i, j];
-          tileNo++;
-        } else if(j%2 === 1 && column == 1){
-            $('.board').append("<div class='tile dark red' id="+tileNo+"></div>");
-            tiles[tileNo] = [i, j];
-            tileNo++;
-        } else if(j%2 ===1){
-            $('.board').append("<div class='tile dark' id="+tileNo+"></div>");
-            tiles[tileNo] = [i, j];
-            tileNo++;
-        } else if(j%2 === 0){
-            $('.board').append("<div class='tile light' id="+tileNo+"></div>");
-            tiles[tileNo] = [i, j];
-            tileNo++;
-        }
-      } if(i%2 === 1){
-          if( j%2 === 0 && column === 2 ) {
+  ],
+  create: function() {
+    let tileNo = 0;
+    let checkerNo = 0;
+    this.board.forEach(function(row, i) {
+      row.forEach(function(column, j) {
+        if (i%2 === 0) {
+          if(j%2 === 1 && column == 2) {
             $('.board').append("<div class='tile dark white' id="+tileNo+"></div>");
             tiles[tileNo] = [i, j];
             tileNo++;
-          } else if(j%2===0 && column === 1) {
+            checkers[checkerNo] = new Checker('white', [i, j]);
+            checkerNo++
+          } else if(j%2 === 1 && column == 1){
               $('.board').append("<div class='tile dark red' id="+tileNo+"></div>");
               tiles[tileNo] = [i, j];
               tileNo++;
-          } else if(j%2===0) {
+              checkers[checkerNo] = new Checker('red', [i, j]);
+              checkerNo++
+          } else if(j%2 ===1){
               $('.board').append("<div class='tile dark' id="+tileNo+"></div>");
               tiles[tileNo] = [i, j];
               tileNo++;
-          } else if(j%2 === 1){
+          } else if(j%2 === 0){
               $('.board').append("<div class='tile light' id="+tileNo+"></div>");
               tiles[tileNo] = [i, j];
               tileNo++;
           }
-      }
+        } if(i%2 === 1){
+            if( j%2 === 0 && column === 2 ) {
+              $('.board').append("<div class='tile dark white' id="+tileNo+"></div>");
+              tiles[tileNo] = [i, j];
+              tileNo++;
+              checkers[checkerNo] = new Checker('white', [i, j]);
+              checkerNo++
+            } else if(j%2===0 && column === 1) {
+                $('.board').append("<div class='tile dark red' id="+tileNo+"></div>");
+                tiles[tileNo] = [i, j];
+                tileNo++;
+                checkers[checkerNo] = new Checker('red', [i, j]);
+                checkerNo++
+            } else if(j%2===0) {
+                $('.board').append("<div class='tile dark' id="+tileNo+"></div>");
+                tiles[tileNo] = [i, j];
+                tileNo++;
+            } else if(j%2 === 1){
+                $('.board').append("<div class='tile light' id="+tileNo+"></div>");
+                tiles[tileNo] = [i, j];
+                tileNo++;
+            }
+        }
+      });
     });
-  });
-  let $whiteChecker = $("<div/>").addClass("white-checker");
-  let $redChecker = $("<div/>").addClass("red-checker");
-  $('.white').append($whiteChecker);
-  $('.red').append($redChecker);
+    let $whiteChecker = $("<div/>").addClass("checker white-checker");
+    let $redChecker = $("<div/>").addClass("checker red-checker");
+    $('.white').append($whiteChecker);
+    $('.red').append($redChecker);
+  }
 }
 
-function selectChecker(checker) {
-  checker.classList.add('isSelected');
+var game = {
+  player1Score: 0,
+  player2Score: 0,
+  turn: 'player1',
+  changeTurns: function(){
+    console.log('change turns');
+  }
 }
+
 
 
